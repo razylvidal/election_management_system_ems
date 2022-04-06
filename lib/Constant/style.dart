d3cd61fc560deb68782664f383696ef1fb40fff9
razylvidal <razylabbygail.vidal@unc.edu.ph>
2022-04-06 23:29:36 +0800

    app bar navigation

---
+++ lib/Constant/style.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color darkBlue = Color(0xFF091C32);
+Color blue = Color(0xFF023047);
+Color mustard = Color(0xFFFFB703);
--- lib/Screens/SCE/hompage.dart
+++ lib/Screens/SCE/hompage.dart
@@ -1,3 +1,7 @@
+
+
+import 'package:election_management_system_ems/Constant/style.dart';
+import 'package:election_management_system_ems/Screens/SCE/login.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,8 +9,96 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    
+    double _width = MediaQuery.of(context).size.width;
+    return Scaffold(
+    appBar: AppBar(
+       backgroundColor:darkBlue,
+          shadowColor: Colors.grey,
+          leadingWidth: 250,
+          toolbarHeight: 100,
+          elevation: 5,
+      leading: Row(
+        children: [
+         /* Padding(
+          padding: EdgeInsets.only(right: 10.0),
+          child: Container(
+            height: 55,
+          child: IconButton(
+          padding: EdgeInsets.only(left: 15),
+              icon: Icon(Icons.menu), 
+
+              onPressed: (){
+
+              }
+          ),
+          ),
+          ),*/
+          Padding(
+            padding: EdgeInsets.only(left: 20.0, right: 50.0),
+            child: InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) => HomePage()));
+              }, // Image tapped
+              splashColor: Colors.white10, // Splash color over image
+              child: Ink.image(
+                //fit: BoxFit.cover, // Fixes border issues
+                width: 150,
+                height: 130,
+                image: AssetImage(
+                  'assets/emsLogo.png',
+                  
+                ),
+              ),
+            ),
+          ),
+      ]
+      ),
+       actions: <Widget>[
+         Icon(Icons.dashboard_rounded, color: mustard,),
+          TextButton(
+            onPressed: () {},
+            child: const Text('Dashboard',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 17.0
+              ),
+              ),
+          ),
+          Padding(padding: EdgeInsets.only(right: 20.0)),
+          Icon(Icons.manage_accounts, color: mustard,),
+          TextButton(
+            onPressed: () {},
+            child: const Text('Accounts',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 17.0
+              ),
+            ),
+          ),
+           Padding(padding: EdgeInsets.only(right: 20.0)),
+          Icon(Icons.exit_to_app, color: mustard,),
+          TextButton(
+            onPressed: () {
+               Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => LoginPage()));
+
+            },
+            child: const Text(
+              'Sign Out',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 17.0,
+              ),
+              
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(right: 20.0)),
+        ],
+    )
     );
   }
 }
