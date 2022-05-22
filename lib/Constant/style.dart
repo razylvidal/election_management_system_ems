cbeb537ed6d1adad5b87118bcd6b667b0c3918a1
razylvidal <razylabbygail.vidal@unc.edu.ph>
2022-05-22 17:45:25 +0800

    update color pallete

--- lib/Constant/style.dart
+++ lib/Constant/style.dart
@@ -3,3 +3,8 @@ import 'package:flutter/material.dart';
 Color darkBlue = Color(0xFF091C32);
 Color blue = Color(0xFF023047);
 Color mustard = Color(0xFFFFB703);
+Color blue1 = Color(0xFF031F4B);
+Color blue2 = Color(0xFF04396C);
+Color blue3 = Color(0xFF035B96);
+Color blue4 = Color(0xFF6497B1);
+Color blue5 = Color(0xFFB3CDE0);
