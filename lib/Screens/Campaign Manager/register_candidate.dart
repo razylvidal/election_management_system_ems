e098536d3d2700fdfc8f5b0b17bb95ff7b0f7949
razylvidal <razylabbygail.vidal@unc.edu.ph>
2022-05-29 00:30:09 +0800

    voting ui

---
+++ lib/Screens/Campaign Manager/register_candidate.dart
@@ -0,0 +1,13 @@
+import 'package:election_management_system_ems/Constant/style.dart';
+import 'package:flutter/material.dart';
+
+class RegisterCandidatePage extends StatelessWidget {
+  const RegisterCandidatePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: blue,
+    );
+  }
+}
